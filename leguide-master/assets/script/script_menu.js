9364c52a99875dcfd679c50a4e01958cb45b2d6d
yayahc <yayahc@localhost.localdomain>
2022-12-16 16:15:49 +0000

    New Code

---
+++ leguide-master/assets/script/script_menu.js
@@ -0,0 +1,13 @@
+// const text = document.querySelector('#text');
+// const card = document.querySelector('#card');
+// const container = document.querySelector('.container');
+// const mapbox = document.querySelector('.mapbox');
+
+// text.addEventListener('click', () => {
+//     container.style.display = 'flex';
+//     mapbox.style.display = 'none';
+// })
+// card.addEventListener('click', () => {
+//     container.style.display = 'none';
+//     mapbox.style.display = 'flex';
+// })
